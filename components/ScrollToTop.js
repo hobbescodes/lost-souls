5f88289e056a995422cec3b877da08736ba9aabd
Blueskunk22 <jakob.hauble@gmail.com>
2022-02-13 19:29:37 -0600

    Update ScrollToTop.js

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -3,12 +3,15 @@ import { ArrowSmUpIcon } from "@heroicons/react/solid";
 
 function ScrollToTop() {
   return (
-    <div className="fixed bottom-4 right-4">
-      <Link href="#top">
-        <a className="inline-flex items-center rounded-full border-2 border-white from-[#14aed0] to-[#6a3fe4] p-1 text-white transition-opacity hover:bg-gradient-to-tr">
-          <ArrowSmUpIcon className="h-6 w-6" />
-        </a>
-      </Link>
+    <div className="fixed bottom-4 right-5">
+      <div className="relative transition-all duration-150 ease-out hover:scale-110 hover:cursor-pointer">
+        <div className="absolute inset-0 rounded-md bg-gradient-to-tr from-[#14aed0] to-[#6a3fe4] blur-lg"></div>
+        <Link href="#top">
+          <a className="relative inline-flex items-center rounded-full bg-black">
+            <ArrowSmUpIcon className="h-8 w-8" />
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
