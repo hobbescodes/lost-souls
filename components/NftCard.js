798117bb75a96e0ca4c1254a46066d5b2e484070
Blueskunk22 <jakob.hauble@gmail.com>
2022-03-04 19:51:16 -0600

    Didnt work, revert lol

--- components/NftCard.js
+++ components/NftCard.js
@@ -64,7 +64,10 @@ function NftCard({ nft }) {
   const tokenDetails = (tokenId) => {
     const options = { method: "GET" };
 
-    fetch(`${process.env.NEXT_PUBLIC_TOKEN_API}${tokenId}`, options)
+    fetch(
+      `${process.env.NEXT_PUBLIC_PROXY_URL}${process.env.NEXT_PUBLIC_TOKEN_API}${tokenId}`,
+      options
+    )
       .then((response) => response.json())
       .then((response) => {
         //console.log(response);
